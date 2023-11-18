0678fdb98af97caa7d530951b304074604d5a97c
Sarjyant Shrestha <sarjyant@gmail.com>
2023-11-18 10:40:42 +0545

    latest changes

--- main.py
+++ main.py
@@ -28,17 +28,20 @@ def search_songs(artist_name, track_name):
 def download_song(id, track_name, artist_name):
     download_url = f"https://slavart-api.gamesdrive.net/api/download/track?id={id}"
     response = requests.get(download_url, stream=True)
+    print(response.status_code)
 
     file_path = os.path.join(
         "downloads", f"{artist_name} - {track_name}.flac")
 
-    total_size = int(response.headers['content-length'])
+    total_size = response.headers
+    print(total_size)
     chunk_size = 1024
 
     with open(file_path, 'wb') as f:
         for chunk in tqdm(iterable=response.iter_content(chunk_size=chunk_size), desc=f"{track_name}", total=total_size/chunk_size, unit='KB'):
             if chunk:
                 f.write(chunk)
+
     return file_path
 
 
@@ -53,7 +56,7 @@ def remove_flac():
     dir_name = "./downloads"
     test = os.listdir(dir_name)
 
-    for item in tqdm(test):
+    for item in test:
         if item.endswith(".flac"):
             os.remove(os.path.join(dir_name, item))
 
@@ -79,22 +82,21 @@ if __name__ == "__main__":
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
         client_id=client_info.SPOTIFY_CLIENT_ID, client_secret=client_info.SPOTIFY_CLIENT_SECRET))
 
-    PLAYLIST_ID = 'https://open.spotify.com/playlist/3cGT5N74Ntqw0zRrQXEQll?si=696732a5868446a5'
-    # input(
-    #     'Enter your playlist link (it must be a public playlist): ')
+    PLAYLIST_ID = input(
+        'Enter your playlist link (it must be a public playlist): ')
     results = sp.playlist_tracks(playlist_id=PLAYLIST_ID)
     tracks = results['items']
     total_items = results['total']
     print(f"Total songs detected: {total_items}")
-    # os.makedirs("downloads", exist_ok=True)
-    # for track in tracks:
-    #     track_name = track['track']['name']
-    #     artist_name = track['track']['artists'][0]['name']
-    #     song_id = search_songs(track_name=track_name, artist_name=artist_name)
-    #     flac_path = download_song(
-    #         id=song_id[0], track_name=song_id[1], artist_name=song_id[2])
-    #     mp3_path = convert_to_mp3(file_path=flac_path)
-    #     get_metadata(path_flac=flac_path, path_mp3=mp3_path)
+    os.makedirs("downloads", exist_ok=True)
+    for track in tracks:
+        track_name = track['track']['name']
+        artist_name = track['track']['artists'][0]['name']
+        song_id = search_songs(track_name=track_name, artist_name=artist_name)
+        flac_path = download_song(
+            id=song_id[0], track_name=song_id[1], artist_name=song_id[2])
+        mp3_path = convert_to_mp3(file_path=flac_path)
+        get_metadata(path_flac=flac_path, path_mp3=mp3_path)
 
-    # print("removing flac..")
-    # remove_flac()
+    print("removing flac..")
+    remove_flac()
